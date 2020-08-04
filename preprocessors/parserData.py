0570d4f2b72351fe293dad396e79ad736b40f466
Eduardo Uliana <edu02_rko@hotmail.com>
2020-08-03 23:04:34 -0300

    Adicionado UPSERT

--- preprocessors/parserData.py
+++ preprocessors/parserData.py
@@ -1,6 +1,6 @@
 def __insertData(draws, conn):
     for draw, numbers in draws.items():
-        conn.execute("INSERT INTO LOTOFACIL (ID, NUMBERS) VALUES (" + str(draw) + ", '" + str(numbers) + "')")
+        conn.execute("INSERT INTO LOTOFACIL (ID, NUMBERS) VALUES (" + str(draw) + ", '" + str(numbers) + "') ON CONFLICT (ID) DO NOTHING")
     conn.commit()
 
 def parser(draws, conn):
