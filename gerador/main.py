3e389a1ecad19f6ae8efbbd6761bd5cceb44a2cb
Eduardo Uliana <edu02_rko@hotmail.com>
2022-10-09 20:23:42 -0300

    Adicionado salvamento dos jogos no banco de dados

--- gerador/main.py
+++ gerador/main.py
@@ -1,6 +1,11 @@
 import itertools as it
 import json
 
+import sys
+sys.path.append('../')
+
+from database.postgresql import PostgreSQL
+
 DEFAULT_DRAW_LENGHT = 15
 MAX_DRAW_NUMBER = 25
 FILE_PATH = "all_combinations_15.json"
@@ -14,10 +19,15 @@ def main():
   all_combinations = list(it.combinations(items, DEFAULT_DRAW_LENGHT))
 
   combinations = {}
+
+  db = PostgreSQL()
   for combination in all_combinations:
     key = ':'.join(str(n) for n in combination)
     combinations[key] = combination
 
+    db.insert_possibilities(key, f"[{','.join(str(n) for n in combination)}]")
+
+  del db
   save_to_file(combinations)
 
 if __name__ == '__main__':
